539a725e26a651f811e31a6e15c88030c60ba1e0
eaparicio <erapariciopuerta@gmail.com>
2019-12-17 12:36:52 +0100

    adding fix for backslashing problem

--- miRQC/urls.py
+++ miRQC/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    url(r'/^$', startNew.as_view(), name='home'),
+    url(r'^/$', startNew.as_view(), name='home'),
     url(r'^check', checkStatus.as_view(), name="check_status"),
     url(r'^testqc', testMulti.as_view()),
     url(r'^launch', launchJob.as_view(), name="launch"),
