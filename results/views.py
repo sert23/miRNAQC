c62e976b55ef2c8d3775751c8ed8c5e56147fa4a
eaparicio <erapariciopuerta@gmail.com>
2020-03-25 16:50:42 +0100

    adding exception

--- results/views.py
+++ results/views.py
@@ -13,6 +13,7 @@ import math
 from random import randrange
 import json
 from collections import OrderedDict
+from newJob.views import parse_web_log
 
 
 with open(VAR_DICT_PATH) as json_file:
@@ -388,90 +389,113 @@ class testMulti(FormView):
 class loadResults(FormView):
 
     def get(self, request):
+        try:
 
-        context = {}
-        path = request.path
-        #get folder from pathresults for job
-        folder = path.split("/")[-1]
-        context["jobID"] = folder
-        context["result_url"] = reverse_lazy("check_status") + "/" + folder
-        context["absolute_url"] = MAIN_SITE
-        comp_folder = os.path.join(MEDIA_ROOT,folder,"query", "comparisons")
-        with open(os.path.join(comp_folder,"config.json")) as d_file:
-            comp_dict = json.load(d_file, object_pairs_hook=OrderedDict)
-        comparison = path.split("/")[-2]
-        if comparison not in comp_dict.keys():
-            comparison = "vsAll"
+            context = {}
+            path = request.path
+            #get folder from pathresults for job
+            folder = path.split("/")[-1]
+            context["jobID"] = folder
+            context["result_url"] = reverse_lazy("check_status") + "/" + folder
+            context["absolute_url"] = MAIN_SITE
+            comp_folder = os.path.join(MEDIA_ROOT,folder,"query", "comparisons")
+            with open(os.path.join(comp_folder,"config.json")) as d_file:
+                comp_dict = json.load(d_file, object_pairs_hook=OrderedDict)
+            comparison = path.split("/")[-2]
+            if comparison not in comp_dict.keys():
+                comparison = "vsAll"
 
-        query_folder = os.path.join(MEDIA_ROOT,folder,"query", "comparisons", comparison)
+            query_folder = os.path.join(MEDIA_ROOT,folder,"query", "comparisons", comparison)
 
-        # comparisons = [[f,comp_dict.get(f)] for f in os.listdir(comp_folder) if
-        #          os.path.isdir(os.path.join(comp_folder, f))]
-        comparisons = [[f, comp_dict.get(f)] for f in comp_dict.keys() if os.path.isdir(os.path.join(comp_folder, f))]
-        # print(comparisons)
-        # query_folder = os.path.join(MEDIA_ROOT,folder,"query",)
-        val_file = os.path.join(query_folder,"value.tsv")
-        perc_file = os.path.join(query_folder,"percentil.tsv")
-        context["vals_link"] = val_file.replace(MEDIA_ROOT,MEDIA_URL)
-        context["perc_link"] = perc_file.replace(MEDIA_ROOT,MEDIA_URL)
+            # comparisons = [[f,comp_dict.get(f)] for f in os.listdir(comp_folder) if
+            #          os.path.isdir(os.path.join(comp_folder, f))]
+            comparisons = [[f, comp_dict.get(f)] for f in comp_dict.keys() if os.path.isdir(os.path.join(comp_folder, f))]
+            # print(comparisons)
+            # query_folder = os.path.join(MEDIA_ROOT,folder,"query",)
+            val_file = os.path.join(query_folder,"value.tsv")
+            perc_file = os.path.join(query_folder,"percentil.tsv")
+            context["vals_link"] = val_file.replace(MEDIA_ROOT,MEDIA_URL)
+            context["perc_link"] = perc_file.replace(MEDIA_ROOT,MEDIA_URL)
 
-        #parameters
-        context["par_tab"] = par_table(os.path.join(MEDIA_ROOT,folder,"query", "comparisons","summary_results.tsv"))
+            #parameters
+            context["par_tab"] = par_table(os.path.join(MEDIA_ROOT,folder,"query", "comparisons","summary_results.tsv"))
 
-        #read files into dfs
-        val_df = pandas.read_csv(val_file, sep="\t")
-        perc_df = pandas.read_csv(perc_file, sep="\t")
-        columns = cols_dict(perc_df)
-        # context["basic_hm"] = plot_heatmap(perc_df)
-        #basic statistics
-        basic_tab,basic_perc = basic_table(val_df, perc_df, columns)
-        context["basic_table"] = basic_tab
-        context["basic_perc"] = basic_perc
-        context["comp_list"] = comparisons
+            #read files into dfs
+            val_df = pandas.read_csv(val_file, sep="\t")
+            perc_df = pandas.read_csv(perc_file, sep="\t")
+            columns = cols_dict(perc_df)
+            # context["basic_hm"] = plot_heatmap(perc_df)
+            #basic statistics
+            basic_tab,basic_perc = basic_table(val_df, perc_df, columns)
+            context["basic_table"] = basic_tab
+            context["basic_perc"] = basic_perc
+            context["comp_list"] = comparisons
 
-        #sequencing yield
-        seqYield_tab, seqYield_perc = tables_yield(val_df, perc_df, columns)
-        context["seqYield_table"] = seqYield_tab
-        context["seqYield_perc"] = seqYield_perc
+            #sequencing yield
+            seqYield_tab, seqYield_perc = tables_yield(val_df, perc_df, columns)
+            context["seqYield_table"] = seqYield_tab
+            context["seqYield_perc"] = seqYield_perc
 
-        # library complexity
-        complex_tab, complex_perc = tables_complex(val_df, perc_df, columns)
-        context["complex_table"] = complex_tab
-        context["complex_perc"] = complex_perc
+            # library complexity
+            complex_tab, complex_perc = tables_complex(val_df, perc_df, columns)
+            context["complex_table"] = complex_tab
+            context["complex_perc"] = complex_perc
 
-        #sequencing quality
-        seqQual_tab, seqQual_perc = seq_qual_tab(val_df, perc_df, columns)
-        context["seqQual_table"] = seqQual_tab
-        context["seqQual_perc"] = seqQual_perc
+            #sequencing quality
+            seqQual_tab, seqQual_perc = seq_qual_tab(val_df, perc_df, columns)
+            context["seqQual_table"] = seqQual_tab
+            context["seqQual_perc"] = seqQual_perc
 
-        # library quality
-        library_table, library_perc = library_tab(val_df, perc_df, columns)
-        context["library_table"] = library_table
-        context["library_perc"] = library_perc
+            # library quality
+            library_table, library_perc = library_tab(val_df, perc_df, columns)
+            context["library_table"] = library_table
+            context["library_perc"] = library_perc
 
-        # contamination
+            # contamination
 
-        cont_table, cont_perc = contamination_tab(val_df, perc_df, columns)
-        context["contam_table"] = cont_table
-        context["contam_perc"] = cont_perc
+            cont_table, cont_perc = contamination_tab(val_df, perc_df, columns)
+            context["contam_table"] = cont_table
+            context["contam_perc"] = cont_perc
 
-        # length distribution
+            # length distribution
 
-        len_table, len_perc = length_tab(val_df, perc_df, columns)
-        context["length_table"] = len_table
-        context["length_perc"] = len_perc
+            len_table, len_perc = length_tab(val_df, perc_df, columns)
+            context["length_table"] = len_table
+            context["length_perc"] = len_perc
 
-        context["sub_site"] = SUB_SITE
-        context["comparison"] = comparison
+            context["sub_site"] = SUB_SITE
+            context["comparison"] = comparison
 
-        # percentiles
-        # print(os.path.join(query_folder,"percentiles"))
-        if os.path.exists(os.path.join(query_folder,"percentiles")):
-            context["perc_list"] = parse_perc_files(query_folder)
-        # t1,t2 = test_table()
-        # context["test_table"] = t1
-        # context["test_table2"] = t2
+            # percentiles
+            # print(os.path.join(query_folder,"percentiles"))
+            if os.path.exists(os.path.join(query_folder,"percentiles")):
+                context["perc_list"] = parse_perc_files(query_folder)
+            # t1,t2 = test_table()
+            # context["test_table"] = t1
+            # context["test_table2"] = t2
 
-        # print("hey")
-        # return render(self.request, 'test_table.html', context)
-        return render(self.request, 'results.html', context)
+            # print("hey")
+            # return render(self.request, 'test_table.html', context)
+            return render(self.request, 'results.html', context)
+        except:
+            context = {}
+            path = request.path
+            # get folder from path
+            finished = False
+            folder = path.split("/")[-1]
+            context["jobID"] = folder
+            context["result_url"] = reverse_lazy("check_status") + "/" + folder
+            context["absolute_url"] = MAIN_SITE
+            logfile = os.path.join(MEDIA_ROOT, folder, "query", "comparisons", "summaryqc.log")
+            if os.path.exists(logfile):
+                launched = True
+            else:
+                launched = False
+
+            if launched:
+                message, finished = parse_web_log(logfile)
+                context["message"] = message
+                return render(self.request, 'status.html', context)
+            else:
+                context["message"] = "Your Job is in queue"
+                return render(self.request, 'status.html', context)
