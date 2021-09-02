86e04df7645e4affd2a41a9f104b7a4a05dff4f1
eaparicio <erapariciopuerta@gmail.com>
2021-09-02 00:50:08 -0400

    .

--- cleaning/clean_upload.py
+++ cleaning/clean_upload.py
@@ -34,38 +34,38 @@ with open(os.path.join(folder_to_clean,"test.txt") , "w") as testfile:
                 except:
                     testfile.write("")
 
-# 5 days
-shorter_days = 86400 * 5
-# temp
-folder_to_clean = os.path.join(sys.argv[1], "temp")
-folders = [x for x in os.listdir(folder_to_clean) if os.path.isdir(os.path.join(folder_to_clean,x))]
-to_rem = [os.path.join(folder_to_clean,x) for x in folders if x not in white_list]
-for dir in to_rem:
-    timestamp = os.path.getmtime(dir)
-    # testfile.write(dir + " " + str(timestamp) +" " + str(now - numdays) + "\n")
-    if now - shorter_days > timestamp:
-        # testfile.write(dir + " " + str(timestamp) + "\n")
-        try:
-            testfile.write(dir + "\n")
-            shutil.rmtree(dir)  # uncomment to use
-            dfile.write(dir + "\n")
-            print(dir)
-        except:
-            testfile.write("")
-# uploaded_files
+    # 5 days
+    shorter_days = 86400 * 5
+    # temp
+    folder_to_clean = os.path.join(sys.argv[1], "temp")
+    folders = [x for x in os.listdir(folder_to_clean) if os.path.isdir(os.path.join(folder_to_clean,x))]
+    to_rem = [os.path.join(folder_to_clean,x) for x in folders if x not in white_list]
+    for dir in to_rem:
+        timestamp = os.path.getmtime(dir)
+        # testfile.write(dir + " " + str(timestamp) +" " + str(now - numdays) + "\n")
+        if now - shorter_days > timestamp:
+            # testfile.write(dir + " " + str(timestamp) + "\n")
+            try:
+                # testfile.write(dir + "\n")
+                shutil.rmtree(dir)  # uncomment to use
+                # dfile.write(dir + "\n")
+                print(dir)
+            except:
+                testfile.write("")
+    # uploaded_files
 
-folder_to_clean = os.path.join(sys.argv[1], "uploaded_files")
-folders = [x for x in os.listdir(folder_to_clean) if os.path.isdir(os.path.join(folder_to_clean,x))]
-to_rem = [os.path.join(folder_to_clean,x) for x in folders if x not in white_list]
-for dir in to_rem:
-    timestamp = os.path.getmtime(dir)
-    # testfile.write(dir + " " + str(timestamp) +" " + str(now - numdays) + "\n")
-    if now - shorter_days > timestamp:
-        # testfile.write(dir + " " + str(timestamp) + "\n")
-        try:
-            testfile.write(dir + "\n")
-            shutil.rmtree(dir)  # uncomment to use
-            dfile.write(dir + "\n")
-            print(dir)
-        except:
-            testfile.write("")
+    folder_to_clean = os.path.join(sys.argv[1], "uploaded_files")
+    folders = [x for x in os.listdir(folder_to_clean) if os.path.isdir(os.path.join(folder_to_clean,x))]
+    to_rem = [os.path.join(folder_to_clean,x) for x in folders if x not in white_list]
+    for dir in to_rem:
+        timestamp = os.path.getmtime(dir)
+        # testfile.write(dir + " " + str(timestamp) +" " + str(now - numdays) + "\n")
+        if now - shorter_days > timestamp:
+            # testfile.write(dir + " " + str(timestamp) + "\n")
+            try:
+                # testfile.write(dir + "\n")
+                shutil.rmtree(dir)  # uncomment to use
+                # dfile.write(dir + "\n")
+                print(dir)
+            except:
+                testfile.write("")
